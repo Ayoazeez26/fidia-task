9d354ffdfbbb7249a1e6baa7f4c879445ad295a3
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2021-08-22 12:54:45 +0100

    made changes to text

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dasboard flex w-screen h-screen">
+  <div class="dashboard flex w-screen h-screen">
     <div class="h-full text-fd-ft text-sm pt-3 pl-4 w-1/5 bg-fd-bgt">
       <div class="head flex mb-8 items-center">
         <div class="store-img bg-white rounded-full flex items-center justify-center p-2">
@@ -61,61 +61,72 @@
       </div>
     </div>
     <div class="h-full w-4/5">
-    <div class="top-search border-b border-fd-border py-4 px-8 relative flex justify-between mb-7 items-center">
-      <img class="absolute mt-0.5 top-5 left-9" src="../assets/img/search.png" alt="search Icon">
-      <input class="ml-6 outline-none :focus:outline-none" type="text" name="search" id="search" placeholder="Search...">
-      <img src="../assets/img/Header icons.png" alt="header icons">
-    </div>
-    <div class="dash-head flex px-8 items-center justify-between">
-      <h1 class="font-bold text-3xl text-fd-ft">Payments</h1>
-      <div class="buttons text-fd-text text-sm font-medium flex items-center justify-between">
-        <div class="fil flex rounded bg-white items-center justify-between">
-          <img src="../assets/img/filter.png" alt="filter icon">
-          <p class="ml-1">Filter</p>
-        </div>
-        <div class="exp ml-2 flex rounded bg-white items-center justify-between">
-          <img src="../assets/img/export.png" alt="export icon">
-          <p class="ml-1">Export</p>
-        </div>
-        <div class="exp ml-2 flex rounded bg-fd-blu items-center justify-between">
-          <img src="../assets/img/payment.png" alt="create payment icon">
-          <p class="ml-1 text-white">Create Payment</p>
+      <div class="top-search border-b border-fd-border py-4 px-8 relative flex justify-between mb-7 items-center">
+        <img class="absolute mt-0.5 top-5 left-9" src="../assets/img/search.png" alt="search Icon">
+        <input class="ml-6 outline-none :focus:outline-none" type="text" name="search" id="search" placeholder="Search...">
+        <img src="../assets/img/Header icons.png" alt="header icons">
+      </div>
+      <div class="dash-head flex px-8 items-center justify-between">
+        <h1 class="font-bold text-3xl text-fd-ft">Payments</h1>
+        <div class="buttons text-fd-text text-sm font-medium flex items-center justify-between">
+          <div class="fil flex rounded bg-white items-center justify-between">
+            <img src="../assets/img/filter.png" alt="filter icon">
+            <p class="ml-1">Filter</p>
+          </div>
+          <div class="exp ml-2 flex rounded bg-white items-center justify-between">
+            <img src="../assets/img/export.png" alt="export icon">
+            <p class="ml-1">Export</p>
+          </div>
+          <div class="exp ml-2 flex rounded bg-fd-blu items-center justify-between">
+            <img src="../assets/img/payment.png" alt="create payment icon">
+            <p class="ml-1 text-white">Create Payment</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div
-      class="header relative text-sm flex justify-start font-semibold items-center mt-5 text-fd-foot border-b border-fd-border w-full"
-    >
-      <h4
-        v-for="(tab, index) in tabs"
-        :key="index"
-        class="cursor-pointer pb-2 px-1 ml-7 relative"
-        :class="{ activeTab: selectedTab === tab }"
-        @click="selectedTab = tab"
+      <div
+        class="header relative text-sm flex justify-start font-semibold items-center mt-5 text-fd-foot border-b border-fd-border w-full"
       >
-        {{ tab }}
-      </h4>
-    </div>
-    <div class="w-full mt-5 px-8">
-      <zk-table
-        ref="table"
-        sum-text="sum"
-        index-text="#"
-        :data="data"
-        :columns="columns"
-        :stripe="props.stripe"
-        :border="props.border"
-        :show-header="props.showHeader"
-        :show-summary="props.showSummary"
-        :show-row-hover="props.showRowHover"
-        :show-index="props.showIndex"
-        :tree-type="props.treeType"
-        :is-fold="props.isFold"
-        :expand-type="props.expandType"
-        :selection-type="props.selectionType">
-        
-      </zk-table>
-    </div>
+        <h4
+          v-for="(tab, index) in tabs"
+          :key="index"
+          class="cursor-pointer pb-2 px-1 ml-7 relative"
+          :class="{ activeTab: selectedTab === tab }"
+          @click="selectedTab = tab"
+        >
+          {{ tab }}
+        </h4>
+      </div>
+      <div class="w-full mt-5 px-8">
+        <zk-table
+          ref="table"
+          sum-text="sum"
+          index-text="#"
+          :data="data"
+          :columns="columns"
+          :stripe="props.stripe"
+          :border="props.border"
+          :show-header="props.showHeader"
+          :show-summary="props.showSummary"
+          :show-row-hover="props.showRowHover"
+          :show-index="props.showIndex"
+          :tree-type="props.treeType"
+          :is-fold="props.isFold"
+          :expand-type="props.expandType"
+          :selection-type="props.selectionType">
+          
+        </zk-table>
+      </div>
+      <div class="tab-after mt-6 px-8 flex items-center justify-between">
+        <p class="res text-fd-text font-normal"><span class="font-medium">16</span> results</p>
+        <div class="buttons text-fd-text text-sm font-medium flex items-center justify-between">
+          <div class="prev flex rounded bg-white items-center justify-center">
+            <p class="ml-1">Previous</p>
+          </div>
+          <div class="nxt ml-2 flex rounded bg-white items-center justify-center">
+            <p class="ml-1">Next</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -241,7 +252,7 @@ export default {
           {
             label: 'AMOUNT',
             prop: 'price',
-            width: '130px',
+            width: '230px',
           },
           {
             label: 'DESCRIPTION',
@@ -250,7 +261,7 @@ export default {
           {
             label: 'CUSTOMER',
             prop: 'name',
-            width: '160px',
+            width: '200px',
           },
           {
             label: 'DATE',
@@ -271,13 +282,16 @@ export default {
     },
   };
 </script>
-<style scoped>
+<style>
+.dashboard {
+  min-height: 100vh;
+}
 .zk-table__body-row {
   height: auto !important;
 }
-.zk-table--level-1-cell {
-  font-size: 14px !important;
-  color: "#1A1F36" !important;
+.zk-table__cell-inner {
+  font-size: 14px;
+  color: #1A1F36;
 }
 .buttons div {
   box-shadow: 0px 2px 5px rgba(60, 66, 87, 0.08), 0px 0px 0px 1px rgba(60, 66, 87, 0.16), 0px 1px 1px rgba(0, 0, 0, 0.12);
@@ -286,5 +300,8 @@ export default {
 .activeTab {
   color: #5469D4;
   border-bottom: 2px solid #5469D4;
+}
+.prev {
+  opacity: 0.5;
 }
 </style>
